f91b7d3ae1df8a5bbad059cad540b6f509332d43
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2024-05-29 10:10:15 +0200

    Tp correction 3

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const express = require('express');
+const express = require("express");
+const uuid = require("uuid");
 
 // instancier le server
 const app = express();
@@ -7,73 +8,99 @@ const app = express();
 app.use(express.json());
 
 // ------------------------------------------------
-// * MOCK DATA
+// * MongoDB
 // ------------------------------------------------
-// Simulation de données en mémoire
-let DB_ARTICLES = [
-    { id: 1, title: 'Premier article', content: 'Contenu du premier article', author: 'Isaac' },
-    { id: 2, title: 'Deuxième article', content: 'Contenu du deuxième article', author: 'Sanchez' },
-    { id: 3, title: 'Troisième article', content: 'Contenu du troisième article', author: 'Toto' }
-];
+const mongoose = require("mongoose");
+
+// SUCCESS : On va ecouter quand il est connecté (on va afficher un message quand connecté)
+mongoose.connection.once("open", () => {
+  console.log("Vous êtes connecté(e) à la base");
+});
+
+// ERROR : Quand la connection ne marche pas
+mongoose.connection.on("error", () => {
+  console.log("Erreur de connexion à la base");
+});
+
+// Lancer la connexion à la bdd mongo
+mongoose.connect("mongodb://127.0.0.1:27017/db_article");
+
+// Modèle Article
+const Article = mongoose.model(
+  "Article",
+  { uid: String, title: String, content: String, author: String },
+  "articles"
+);
 
 // ------------------------------------------------
 // * ROUTES
 // ------------------------------------------------
-app.get('/articles', (request, response) => {
-    return response.json(DB_ARTICLES);
+app.get("/articles", async (request, response) => {
+  // Récupérer les articles en base
+  const articles = await Article.find();
+
+  return response.json(articles);
 });
 
-app.get('/article/:id', (request, response) => {
+app.get("/article/:uid", async (request, response) => {
+  // récupérer un param d'url
+  const uidParam = request.params.uid;
 
-    // récupérer un param d'url
-    const id = parseInt(request.params.id);
+  // Essayer de trouver un article avec l'uid X
+  const foundArticle = await Article.findOne({ uid: uidParam });
 
-    // Essayer de trouver un article avec l'id X
-    const foundArticle = DB_ARTICLES.find(article => article.id === id);
-
-    return response.json(foundArticle);
+  return response.json(foundArticle);
 });
 
-app.post('/save-article', (request, response) => {
-    // Essaye de récupérer un article existant (à l'aide des données envoyées)
-    const articleJSON = request.body;
+app.post("/save-article", async (request, response) => {
+  // Essaye de récupérer un article existant (à l'aide des données envoyées)
+  const articleJSON = request.body;
 
-    // Si l'article existe => Le code pour faire un update/mise à jour
-    if (articleJSON.id){
+  // Si l'article existe => Le code pour faire un update/mise à jour
+  if (articleJSON.uid) {
+    // Trouve un article dans la liste (c'est récupérer l'adresse de l'objet)
+    const foundArticle = await Article.findOne({ uid: articleJSON.uid });
 
-        // Trouve un article dans la liste (c'est récupérer l'adresse de l'objet)
-        const foundArticle = DB_ARTICLES.find(article => article.id === articleJSON.id);
+    if (foundArticle) {
+      foundArticle.title = articleJSON.title;
+      foundArticle.content = articleJSON.content;
+      foundArticle.author = articleJSON.author;
 
-        if (foundArticle) {
-            foundArticle.title = articleJSON.title;
-            foundArticle.content = articleJSON.content;
+      // Persister/Save en base
+      await foundArticle.save();
 
-            return response.json("Article modifié avec succès");
-        }
+      return response.json("Article modifié avec succès");
     }
-    // Sinon faire le code pour creer/ajouter un article
-    // -- si jamais voulais simuler un auto increment id
-    // articleJSON.id = l'algo pour auto increment
-    DB_ARTICLES.push(articleJSON);
+  }
+  // 2 :: CREATION
+  // Sinon faire le code pour creer/ajouter un article
+  const article = new Article(articleJSON); // pas encore base (crée en mémoire)
 
-    return response.json("Article crée avec succès");
+  // generer l'uid
+  article.uid = uuid.v4();
+
+  // persister/save en base
+  await article.save();
+
+  return response.json("Article crée avec succès");
 });
 
-app.delete('/delete-article/:id', (request, response) => {
-    // récupérer un param d'url
-    const id = parseInt(request.params.id);
+app.delete("/delete-article/:uid", async (request, response) => {
+  // récupérer un param d'url
+  const uidParam = request.params.uid;
 
-    // chercher index du tableau quand l'id d'un article = l'id de l'url
-    const foundIndex = DB_ARTICLES.findIndex(article => article.id === id);
+  // Récupérer l'article
+  const article = await Article.findOne({ uid: uidParam });
 
-    // supprimer depuis l'index
-    DB_ARTICLES.splice(foundIndex, 1);
+  // Je supprime que si il existe
+  if (article) {
+    await article.deleteOne();
+  }
 
-    return response.json(`Article supprimé`);
+  return response.json(`Article supprimé`);
 });
-
 
 // Lancer le serveur
 app.listen(3000, () => {
-    console.log('Le serveur est lancé');
+  console.log("Le serveur est lancé");
 });
